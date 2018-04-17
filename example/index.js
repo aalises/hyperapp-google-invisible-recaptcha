663cd1727ebb0b0bf67677ed060faa408ebba7d1
Albert Alises <albert@mint-labs.com>
2018-04-17 12:40:04 +0200

    Fixed linting issues, better default props definition

--- example/index.js
+++ example/index.js
@@ -1,4 +1,4 @@
 import { app } from 'hyperapp';
 import view from './components/Main';
 
-const {} = app(null,null,view,document.body);
+app(null, null, view, document.body);
--- src/InvisibleRecaptcha.js
+++ src/InvisibleRecaptcha.js
@@ -4,66 +4,64 @@ import uuid from 'uuid/v4';
 const renderers = [];
 let container = null;
 
-const injectScript = locale => {
+const injectScript = (locale) => {
   window.GoogleRecaptchaLoaded = () => {
-    while ( renderers.length ) {
+    while (renderers.length) {
       const renderer = renderers.pop();
       renderer();
     }
-};
+  };
 
-const script = document.createElement( 'script' );
+  const script = document.createElement('script');
   script.id = 'recaptcha';
   script.src = `https://www.google.com/recaptcha/api.js?hl=${locale}&onload=GoogleRecaptchaLoaded&render=explicit`;
   script.type = 'text/javascript';
   script.async = true;
   script.defer = true;
-  script.onerror = function( error ) { throw error; };
-  document.body.appendChild( script );
+  script.onerror = function (error) { throw error; };
+  document.body.appendChild(script);
 };
 
-const createRecaptcha = (props,el) => {
-    let { sitekey, locale, badge, onResolved, onLoaded, onInstanceCreated } = props;
-    /* Default prop values if undefined */
-    if(!locale) locale = 'en';
-    if(!badge) badge = 'bottomright';
-    if(!onLoaded) onLoaded = function onLoaded() {};
+const createRecaptcha = (props) => {
+  const {
+    sitekey, locale = 'en', badge = 'bottomright', onResolved, onLoaded = function onLoaded() {}, onInstanceCreated,
+  } = props;
 
-    let callbackName = 'GoogleRecaptchaResolved-' + uuid();
-    window[ callbackName ] = onResolved;
+  const callbackName = `GoogleRecaptchaResolved-${uuid()}`;
+  window[callbackName] = onResolved;
 
-    /* Instantiates the recaptcha */
-    const loaded = () => {
-        if ( container ) {
-          const recaptchaId = window.grecaptcha.render( container, {
-            sitekey,
-            size: 'invisible',
-            badge,
-            callback: callbackName
-          });
-          
-          /* Recaptcha functions */
-          onInstanceCreated({
-            execute: () => window.grecaptcha.execute(recaptchaId),
-            reset: () => window.grecaptcha.reset(recaptchaId),
-            getResponse: () => window.grecaptcha.getResponse(recaptchaId)
-          })
+  /* Instantiates the recaptcha */
+  const loaded = () => {
+    if (container) {
+      const recaptchaId = window.grecaptcha.render(container, {
+        sitekey,
+        size: 'invisible',
+        badge,
+        callback: callbackName,
+      });
 
-          onLoaded();
-        }
-    };
+      /* Recaptcha functions */
+      onInstanceCreated({
+        execute: () => window.grecaptcha.execute(recaptchaId),
+        reset: () => window.grecaptcha.reset(recaptchaId),
+        getResponse: () => window.grecaptcha.getResponse(recaptchaId),
+      });
 
-    if ( window.grecaptcha ) {
-        loaded();
-    } else {
-        renderers.push( loaded );
-        if ( !document.querySelector( '#recaptcha' ) ) {
-          injectScript( locale );
-        }
+      onLoaded();
     }
-}
+  };
+
+  if (window.grecaptcha) {
+    loaded();
+  } else {
+    renderers.push(loaded);
+    if (!document.querySelector('#recaptcha')) {
+      injectScript(locale);
+    }
+  }
+};
 
 
-export default (props) => (
-  h("div",{oncreate: el => { container = el; createRecaptcha(props,el);}},[])
+export default props => (
+  h('div', { oncreate: (el) => { container = el; createRecaptcha(props); } }, [])
 );
